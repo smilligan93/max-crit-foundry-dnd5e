27232852f9a81d408eb4471a8ac5e95a869f16b3
Shawn Milligan <smilligan93@gmail.com>
2020-01-13 21:38:07 -0500

    update for dnd5e 0.8 and foundry 0.4.4

--- max-crit.js
+++ max-crit.js
@@ -1,86 +1,92 @@
 import { Dice5e } from "../../systems/dnd5e/module/dice.js";
 
-const newDamageRoll = ({event={}, parts, actor, data, template, title, speaker, flavor, critical=true, onClose, dialogOptions}) => {
-  // Inner roll function
-  let rollMode = game.settings.get("core", "rollMode");
-  let roll = crit => {
-    let flav = ( flavor instanceof Function ) ? flavor(parts, data) : title;
-    if ( crit === true ) {
-      let regex = /\d+d\d+/;
-      parts.forEach(part => {
-          console.log(part);
+export class CustomDice5e {
+  static async damageRoll({event={}, parts, actor, data, template, title, speaker, flavor, critical=true, onClose, dialogOptions}) {
+    
+    // Handle input arguments
+    flavor = flavor || title;
+    const rollMode = game.settings.get("core", "rollMode");
+    let rolled = false;
+    
+    // Define inner roll function
+    const _roll = function(parts, crit, form) {
+      // Modify the damage formula for critical hits
+      if ( crit === true ) {
+        let regex = /\d+d\d+/;
+        parts.forEach(part => {
           if (regex.test(part)) {
-              let gRegex = /\d+d\d+/g;
-              let match = part.match(gRegex);
-              match.forEach(m => {
-                  console.log(m);
-                  let split = m.split('d');
-                  if (split.length === 2) {
-                      parts.push(split[0] * split[1]);
-                  }
-              });
+            let gRegex = /\d+d\d+/g;
+            let match = part.match(gRegex);
+            match.forEach((m, index) => {
+                let split = m.split('d');
+                console.log(split);
+                if (split.length === 2) {
+                  let count = Number(split[0]);
+                  let val = Number(split[1]);
+                  if (index === 0 && actor && actor.getFlag("dnd5e", "savageAttacks")) count += 1;
+                  parts.push(count * val);
+                }
+            });
           }
+        });
+        flavor = `${title} (Critical)`;
+      }
+
+      let roll = new Roll(parts.join("+"), data);
+    
+      // Include bonus
+      data['bonus'] = form ? form.find('[name="bonus"]').val() : 0;
+    
+      // Convert the roll to a chat message
+      rolled = true;
+      return roll.toMessage({
+        speaker: speaker,
+        flavor: flavor,
+        rollMode: form ? form.find('[name="rollMode"]').val() : rollMode
       });
-      flav = `${title} (Critical)`;
-    }
-
-    let roll = new Roll(parts.join("+"), data);
-    // roll.alter(add, mult);
-
-    // Execute the roll and send it to chat
-    roll.toMessage({
-      speaker: speaker,
-      flavor: flav,
-      rollMode: rollMode
-    });
-
-    // Return the Roll object
-    return roll;
-  };
-
-  // Modify the roll and handle fast-forwarding
-  if ( event.shiftKey || event.ctrlKey || event.metaKey || event.altKey )  return roll(event.altKey);
-  else parts = parts.concat(["@bonus"]);
-
-  // Construct dialog data
-  template = template || "systems/dnd5e/templates/chat/roll-dialog.html";
-  let dialogData = {
-    formula: parts.join(" + "),
-    data: data,
-    rollMode: rollMode,
-    rollModes: CONFIG.rollModes
-  };
-  
-  // Render modal dialog
-  let crit = false;
-  return new Promise(resolve => {
-    renderTemplate(template, dialogData).then(dlg => {
+    };
+    
+    // Modify the roll and handle fast-forwarding
+    if ( event.shiftKey || event.ctrlKey || event.metaKey || event.altKey ) return _roll(parts, event.altKey);
+    else parts = parts.concat(["@bonus"]);
+    
+    // Render modal dialog
+    template = template || "systems/dnd5e/templates/chat/roll-dialog.html";
+    let dialogData = {
+      formula: parts.join(" + "),
+      data: data,
+      rollMode: rollMode,
+      rollModes: CONFIG.rollModes
+    };
+    const html = await renderTemplate(template, dialogData);
+    
+    // Create the Dialog window
+    let roll;
+    return new Promise(resolve => {
       new Dialog({
         title: title,
-        content: dlg,
+        content: html,
         buttons: {
           critical: {
             condition: critical,
             label: "Critical Hit",
-            callback: () => crit = true
+            callback: html => roll = _roll(parts, true, html)
           },
           normal: {
             label: critical ? "Normal" : "Roll",
+            callback: html => roll = _roll(parts, false, html)
           },
         },
         default: "normal",
         close: html => {
           if (onClose) onClose(html, parts, data);
-          rollMode = html.find('[name="rollMode"]').val();
-          data['bonus'] = html.find('[name="bonus"]').val();
-          let r = roll(crit);
-          resolve(r);
+          resolve(rolled ? roll : false);
         }
       }, dialogOptions).render(true);
     });
-  });
-};
+  }
+}
 
 Hooks.on("ready", () => {
-    Dice5e.damageRoll = newDamageRoll;
+    Dice5e.damageRoll = CustomDice5e.damageRoll;
 });
