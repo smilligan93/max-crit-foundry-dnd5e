5e4c2a621208981adee2e647568d992107224848
Shawn Milligan <smilligan93@gmail.com>
2020-04-24 12:37:15 -0400

    update for compatibility with dnd5e v0.88

--- max-crit.js
+++ max-crit.js
@@ -1,16 +1,43 @@
 import { Dice5e } from "../../systems/dnd5e/module/dice.js";
 
 export class CustomDice5e {
-  static async damageRoll({event={}, parts, actor, data, template, title, speaker, flavor, critical=true, onClose, dialogOptions}) {
-    
+  /* -------------------------------------------- */
+
+  /**
+   * A standardized helper function for managing core 5e "d20 rolls"
+   *
+   * Holding SHIFT, ALT, or CTRL when the attack is rolled will "fast-forward".
+   * This chooses the default options of a normal attack with no bonus, Critical, or no bonus respectively
+   *
+   * @param {Array} parts           The dice roll component parts, excluding the initial d20
+   * @param {Actor} actor           The Actor making the damage roll
+   * @param {Object} data           Actor or item data against which to parse the roll
+   * @param {Event|object}[event    The triggering event which initiated the roll
+   * @param {string} rollMode       A specific roll mode to apply as the default for the resulting roll
+   * @param {String} template       The HTML template used to render the roll dialog
+   * @param {String} title          The dice roll UI window title
+   * @param {Object} speaker        The ChatMessage speaker to pass when creating the chat
+   * @param {string} flavor         Flavor text to use in the posted chat message
+   * @param {boolean} allowCritical Allow the opportunity for a critical hit to be rolled
+   * @param {Boolean} critical      Flag this roll as a critical hit for the purposes of fast-forward rolls
+   * @param {Boolean} fastForward   Allow fast-forward advantage selection
+   * @param {Function} onClose      Callback for actions to take when the dialog form is closed
+   * @param {Object} dialogOptions  Modal dialog options
+   *
+   * @return {Promise}              A Promise which resolves once the roll workflow has completed
+   */
+  static async damageRoll({parts, actor, data, event={}, rollMode=null, template, title, speaker, flavor,
+                            allowCritical=true, critical=false, fastForward=null, onClose, dialogOptions}) {
+
     // Handle input arguments
     flavor = flavor || title;
-    const rollMode = game.settings.get("core", "rollMode");
+    speaker = speaker || ChatMessage.getSpeaker();
+    rollMode = game.settings.get("core", "rollMode");
     let rolled = false;
 
     // Define inner roll function
     const _roll = function(parts, crit, form) {
-      let bonus = form ? form.find('[name="bonus"]').val() : 0;
+      let bonus = form ? form.bonus.value : 0;
 
       // Modify the damage formula for critical hits
       if ( crit === true ) {
@@ -20,13 +47,13 @@ export class CustomDice5e {
             let gRegex = /\d+d\d+/g;
             let match = part.match(gRegex);
             match.forEach((m, index) => {
-                let split = m.split('d');
-                if (split.length === 2) {
-                  let count = Number(split[0]);
-                  let val = Number(split[1]);
-                  if (index === 0 && actor && actor.getFlag("dnd5e", "savageAttacks")) count += 1;
-                  parts.push(count * val);
-                }
+              let split = m.split('d');
+              if (split.length === 2) {
+                let count = Number(split[0]);
+                let val = Number(split[1]);
+                if (index === 0 && actor && actor.getFlag("dnd5e", "savageAttacks")) count += 1;
+                parts.push(count * val);
+              }
             });
           }
         });
@@ -34,35 +61,40 @@ export class CustomDice5e {
           let gRegex = /\d+d\d+/g;
           let match = bonus.match(gRegex);
           match.forEach((m, index) => {
-              let split = m.split('d');
-              if (split.length === 2) {
-                let count = Number(split[0]);
-                let val = Number(split[1]);
-                bonus += `+${count * val}`;
-              }
+            let split = m.split('d');
+            if (split.length === 2) {
+              let count = Number(split[0]);
+              let val = Number(split[1]);
+              bonus += `+${count * val}`;
+            }
           });
         }
-        flavor = `${title} (Critical)`;
+        flavor = `${flavor} (${game.i18n.localize("DND5E.Critical")})`;
       }
 
       data['bonus'] = bonus;
       // Include bonus
       let roll = new Roll(parts.join("+"), data);
-    
+
+      rollMode = form ? form.rollMode.value : rollMode;
       // Convert the roll to a chat message
       roll.toMessage({
         speaker: speaker,
         flavor: flavor,
-        rollMode: form ? form.find('[name="rollMode"]').val() : rollMode
-      });
+      }, { rollMode });
       rolled = true;
       return roll;
     };
-    
+
+    // Determine whether the roll can be fast-forward
+    if ( fastForward === null ) {
+      fastForward = event && (event.shiftKey || event.altKey || event.ctrlKey || event.metaKey);
+    }
+
     // Modify the roll and handle fast-forwarding
-    if ( event.shiftKey || event.ctrlKey || event.metaKey || event.altKey ) return _roll(parts, event.altKey);
+    if ( fastForward ) return _roll(parts, critical || event.altKey);
     else parts = parts.concat(["@bonus"]);
-    
+
     // Render modal dialog
     template = template || "systems/dnd5e/templates/chat/roll-dialog.html";
     let dialogData = {
@@ -72,7 +104,7 @@ export class CustomDice5e {
       rollModes: CONFIG.rollModes
     };
     const html = await renderTemplate(template, dialogData);
-    
+
     // Create the Dialog window
     let roll;
     return new Promise(resolve => {
@@ -81,13 +113,13 @@ export class CustomDice5e {
         content: html,
         buttons: {
           critical: {
-            condition: critical,
-            label: "Critical Hit",
-            callback: html => roll = _roll(parts, true, html)
+            condition: allowCritical,
+            label: game.i18n.localize("DND5E.CriticalHit"),
+            callback: html => roll = _roll(parts, true, html[0].children[0])
           },
           normal: {
-            label: critical ? "Normal" : "Roll",
-            callback: html => roll = _roll(parts, false, html)
+            label: game.i18n.localize(allowCritical ? "DND5E.Normal" : "DND5E.Roll"),
+            callback: html => roll = _roll(parts, false, html[0].children[0])
           },
         },
         default: "normal",
