befaa625a35208e9bc86304598bf5295983092be
Shawn Milligan <smilligan93@gmail.com>
2020-08-27 19:00:52 -0400

    update for latest version of dnd5e

--- max-crit.js
+++ max-crit.js
@@ -4,37 +4,58 @@ import Item5e from "../../systems/dnd5e/module/item/entity.js";
 
 // Override rollDamage on Item5e. This is copy-pasta, the only changes are to `damageRoll` below
 Item5e.prototype.rollDamage = function({event, spellLevel=null, versatile=false}={}) {
-  console.log('HELLO WORLD');
   const itemData = this.data.data;
   const actorData = this.actor.data.data;
   if ( !this.hasDamage ) {
     throw new Error("You may not make a Damage Roll with this Item.");
   }
+  const messageData = {"flags.dnd5e.roll": {type: "damage", itemId: this.id }};
+
+  // Get roll data
   const rollData = this.getRollData();
   if ( spellLevel ) rollData.item.level = spellLevel;
 
+  // Get message labels
+  const title = `${this.name} - ${game.i18n.localize("DND5E.DamageRoll")}`;
+  let flavor = this.labels.damageTypes.length ? `${title} (${this.labels.damageTypes})` : title;
+
   // Define Roll parts
   const parts = itemData.damage.parts.map(d => d[0]);
-  if ( versatile && itemData.damage.versatile ) parts[0] = itemData.damage.versatile;
+
+  // Adjust damage from versatile usage
+  if ( versatile && itemData.damage.versatile ) {
+    parts[0] = itemData.damage.versatile;
+    messageData["flags.dnd5e.roll"].versatile = true;
+  }
+
+  // Scale damage from up-casting spells
   if ( (this.data.type === "spell") ) {
     if ( (itemData.scaling.mode === "cantrip") ) {
-      const lvl = this.actor.data.type === "character" ? actorData.details.level : actorData.details.spellLevel;
-      this._scaleCantripDamage(parts, lvl, itemData.scaling.formula );
-    } else if ( spellLevel && (itemData.scaling.mode === "level") && itemData.scaling.formula ) {
-      this._scaleSpellDamage(parts, itemData.level, spellLevel, itemData.scaling.formula );
+      const level = this.actor.data.type === "character" ? actorData.details.level : actorData.details.spellLevel;
+      this._scaleCantripDamage(parts, itemData.scaling.formula, level, rollData);
+    }
+    else if ( spellLevel && (itemData.scaling.mode === "level") && itemData.scaling.formula ) {
+      const scaling = itemData.scaling.formula;
+      this._scaleSpellDamage(parts, itemData.level, spellLevel, scaling, rollData);
     }
   }
 
   // Define Roll Data
-  const actorBonus = actorData.bonuses[itemData.actionType] || {};
+  const actorBonus = getProperty(actorData, `bonuses.${itemData.actionType}`) || {};
   if ( actorBonus.damage && parseInt(actorBonus.damage) !== 0 ) {
     parts.push("@dmg");
     rollData["dmg"] = actorBonus.damage;
   }
 
+  // Ammunition Damage
+  if ( this._ammo ) {
+    parts.push("@ammo");
+    rollData["ammo"] = this._ammo.data.data.damage.parts.map(p => p[0]).join("+");
+    flavor += ` [${this._ammo.name}]`;
+    delete this._ammo;
+  }
+
   // Call the roll helper utility
-  const title = `${this.name} - Damage Roll`;
-  const flavor = this.labels.damageTypes.length ? `${title} (${this.labels.damageTypes})` : title;
   return damageRoll({
     event: event,
     parts: parts,
@@ -47,19 +68,22 @@ Item5e.prototype.rollDamage = function({event, spellLevel=null, versatile=false}
       width: 400,
       top: event ? event.clientY - 80 : null,
       left: window.innerWidth - 710
-    }
+    },
+    messageData
   });
 }
 
+export async function damageRoll({parts, actor, data, event={}, rollMode=null, template, title, speaker, flavor,
+                                   allowCritical=true, critical=false, fastForward=null, dialogOptions, chatMessage=true, messageData={}}={}) {
 
- // Copy of damageRoll from dice.js, but changes the _roll function to use max value on a crit dice
-async function damageRoll({parts, actor, data, event={}, rollMode=null, template, title, speaker, flavor,
-                          allowCritical=true, critical=false, fastForward=null, onClose, dialogOptions}) {
-  // Handle input arguments
-  flavor = flavor || title;
-  speaker = speaker || ChatMessage.getSpeaker();
-  rollMode = game.settings.get("core", "rollMode");
-  let rolled = false;
+  // Prepare Message Data
+  messageData.flavor = flavor || title;
+  messageData.speaker = speaker || ChatMessage.getSpeaker();
+  const messageOptions = {rollMode: rollMode || game.settings.get("core", "rollMode")};
+  parts = parts.concat(["@bonus"]);
+  fastForward = fastForward ?? (event && (event.shiftKey || event.altKey || event.ctrlKey || event.metaKey));
+
+  /// BEGIN CUSTOM
 
   // Define inner roll function
   const _roll = function(parts, crit, form) {
@@ -106,66 +130,39 @@ async function damageRoll({parts, actor, data, event={}, rollMode=null, template
           }
         });
       }
-      flavor = `${flavor} (${game.i18n.localize("DND5E.Critical")})`;
+      messageData.flavor += ` (${game.i18n.localize("DND5E.Critical")})`;
+      if ( "flags.dnd5e.roll" in messageData ) messageData["flags.dnd5e.roll"].critical = true;
     }
 
     data['bonus'] = bonus;
+    // if (!data["bonus"]) parts.pop();
     // Include bonus
     let roll = new Roll(parts.join("+"), data);
 
-    rollMode = form ? form.rollMode.value : rollMode;
-    // Convert the roll to a chat message
-    roll.toMessage({
-      speaker: speaker,
-      flavor: flavor,
-    }, { rollMode });
-    rolled = true;
-    return roll;
+    if (form) {
+      messageOptions.rollMode = form.rollMode.value;
+    }
+
+    // Execute the roll
+    try {
+      return roll.roll();
+    } catch(err) {
+      console.error(err);
+      ui.notifications.error(`Dice roll evaluation failed: ${err.message}`);
+      return null;
+    }
   };
 
-  // Determine whether the roll can be fast-forward
-  if ( fastForward === null ) {
-    fastForward = event && (event.shiftKey || event.altKey || event.ctrlKey || event.metaKey);
-  }
+  /// END CUSTOM
 
-  // Modify the roll and handle fast-forwarding
-  if ( fastForward ) return _roll(parts, critical || event.altKey);
-  else parts = parts.concat(["@bonus"]);
-
-  // Render modal dialog
-  template = template || "systems/dnd5e/templates/chat/roll-dialog.html";
-  let dialogData = {
-    formula: parts.join(" + "),
-    data: data,
-    rollMode: rollMode,
-    rollModes: CONFIG.rollModes
-  };
-  const html = await renderTemplate(template, dialogData);
-
-  // Create the Dialog window
-  let roll;
-  return new Promise(resolve => {
-    new Dialog({
-      title: title,
-      content: html,
-      buttons: {
-        critical: {
-          condition: allowCritical,
-          label: game.i18n.localize("DND5E.CriticalHit"),
-          callback: html => roll = _roll(parts, true, html[0].children[0])
-        },
-        normal: {
-          label: game.i18n.localize(allowCritical ? "DND5E.Normal" : "DND5E.Roll"),
-          callback: html => roll = _roll(parts, false, html[0].children[0])
-        },
-      },
-      default: "normal",
-      close: html => {
-        if (onClose) onClose(html, parts, data);
-        resolve(rolled ? roll : false);
-      }
-    }, dialogOptions).render(true);
+  // Create the Roll instance
+  const roll = fastForward ? _roll(parts, critical || event.altKey) : await _damageRollDialog({
+    template, title, parts, data, allowCritical, rollMode: messageOptions.rollMode, dialogOptions, roll: _roll
   });
+
+  // Create a Chat Message
+  if ( roll && chatMessage ) roll.toMessage(messageData, messageOptions);
+  return roll;
 }
 
 Hooks.on("ready", () => {
@@ -187,4 +184,41 @@ Hooks.on("ready", () => {
     default: true,
   });
 });
+
+// DIRECTLY COPIED FROM dice.js
+/* -------------------------------------------- */
+
+async function _damageRollDialog({template, title, parts, data, allowCritical, rollMode, dialogOptions, roll}={}) {
+
+  // Render modal dialog
+  template = template || "systems/dnd5e/templates/chat/roll-dialog.html";
+  let dialogData = {
+    formula: parts.join(" + "),
+    data: data,
+    rollMode: rollMode,
+    rollModes: CONFIG.Dice.rollModes
+  };
+  const html = await renderTemplate(template, dialogData);
+
+  // Create the Dialog window
+  return new Promise(resolve => {
+    new Dialog({
+      title: title,
+      content: html,
+      buttons: {
+        critical: {
+          condition: allowCritical,
+          label: game.i18n.localize("DND5E.CriticalHit"),
+          callback: html => resolve(roll(parts, true, html[0].querySelector("form")))
+        },
+        normal: {
+          label: game.i18n.localize(allowCritical ? "DND5E.Normal" : "DND5E.Roll"),
+          callback: html => resolve(roll(parts, false, html[0].querySelector("form")))
+        },
+      },
+      default: "normal",
+      close: () => resolve(null)
+    }, dialogOptions).render(true);
+  });
+}
 
